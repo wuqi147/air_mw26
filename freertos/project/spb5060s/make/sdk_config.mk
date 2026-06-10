2a7bea2bfee887476f30a0cc91e418d819e95b2f
wuqi <2960004339@qq.com>
2026-06-10 19:57:55 +0800

    spb5060s 修复宏定义被覆盖问题

--- freertos/project/spb5060s/make/sdk_config.mk
+++ freertos/project/spb5060s/make/sdk_config.mk
@@ -2,13 +2,13 @@
 # SDK configuration for SPB-5060S (AN8855M + AN8801SB + SK49145B)
 ################################################################################
 # Switch chips
-ENABLED_SWITCH_CHIPS := an8855m
+override ENABLED_SWITCH_CHIPS := an8855m
 
 # PHY chips
-ENABLED_PHY_CHIPS    := an8801sb
+override ENABLED_PHY_CHIPS    := an8801sb
 
 # POE chips
-ENABLED_POE_CHIPS    := sk49145b
+override ENABLED_POE_CHIPS    := sk49145b
 
 # I2C bitbang for PSE
-AIR_EN_I2C_BITBANG   := ENABLED
+override AIR_EN_I2C_BITBANG   := ENABLED
